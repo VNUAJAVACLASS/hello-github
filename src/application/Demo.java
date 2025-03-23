0b26dc104027151939f0ffb5e8be3f05111ee7a3
Nguyen Duc Hoan <nguyenduchoanf01012005@gmail.com>
2025-03-23 10:33:19 +0700

    NguyenDucHoan

--- src/application/Demo.java
+++ src/application/Demo.java
@@ -3,6 +3,6 @@ package application;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Hello Github!");
-		System.out.println("i'm lam");
+		System.out.println("i'm Hoan");
 	}
 }
