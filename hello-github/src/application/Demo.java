e99a1362d7581da15012f8e2beddd9d593608c7d
thanhtuoitre <tvuongnhat@gmail.com>
2025-03-25 21:35:45 +0700

    first commit

--- hello-github/src/application/Demo.java
+++ hello-github/src/application/Demo.java
@@ -3,6 +3,6 @@ package application;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Hello Github!");
-		System.out.println("i'm Hoan");
+		System.out.println("Toi la Thanh");
 	}
 }
